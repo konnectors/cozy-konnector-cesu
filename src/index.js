0b76a297ead6a143236db9ef574a0c05d66be69f
LucsT <lucas@cozycloud.cc>
2018-04-03 18:07:41 +0200

    First version with login and cesu number extraction

--- src/index.js
+++ src/index.js
@@ -5,27 +5,33 @@ const {
   saveFiles,
   addData
 } = require('cozy-konnector-libs')
-const request = requestFactory({ cheerio: true, jar: true, debug: true })
+let request = requestFactory()
+const j = request.jar()
+request = requestFactory({ cheerio: false, jar: j, debug: false })
 
-const baseUrl = 'https://www.cesu.urssaf.fr/info/'
-const dashboardUrl = baseUrl + 'acceuil.html'
-const loginUrl = baseUrl + 'accueil.login.do'
+const baseUrl = 'https://www.cesu.urssaf.fr/'
+const loginUrl = baseUrl + 'info/accueil.login.do'
+const bulletinsUrl = baseUrl + 'decla/index.html?page=page_empl_bulletins_de_salaire&LANG=FR'
+
+
+const dashboardUrl = baseUrl + 'info/accueil.html'
+const menu = 'decla/index.html?page=page_empl_tableau_bord&LANG=FR'
 
 module.exports = new BaseKonnector(start)
 
 function start(fields) {
-  return authenticatefake(fields.login, fields.password)
-  //    .then(printjson)
-}
+  return authenticate(fields.login, fields.password)
+    .then(response => getCesuNumber(response))
+    .then(cesuNum => getBulletinsList(cesuNum))
+    .then(jsonList => constructList(jsonList))
 
-function printjson($) {
-  //console.log($.html())
-  console.log('TRY')
+//    .then (getBulletins)
 }
 
 function authenticate(login, password) {
   log('info', 'Authenticating...')
   return request({
+    resolveWithFullResponse: true,
     method: 'POST',
     uri: loginUrl,
     form: {
@@ -35,50 +41,28 @@ function authenticate(login, password) {
   })
 }
 
-function authenticatefake(login, password) {
-  log('info', 'Authenticating...')
-  return request(
-    {
-      method: 'GET',
-      uri: dashboardUrl
-    } //).then( $ => { console.log($.html()) }
-  )
+function getCesuNumber(response) {
+  const infoCookie = j.getCookies(loginUrl).find(cookie => cookie.key === 'EnligneInfo')
+  var cesuNumMatch = infoCookie.value.match('%22numerocesu%22%3A%22(.+?)%22')
+  if (cesuNumMatch) {
+    log('info','Cesu number found in page')
+    return cesuNumMatch[1]
+  } else throw new Error('')  //TODO
 }
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-function start2(fields) {
-  // The BaseKonnector instance expects a Promise as return of the function
-  return request(`${baseUrl}/index.html`).then($ => {
-    // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-    // here I do an Array.from to convert the cheerio fake array to a real js array.
-    const entries = Array.from($('article')).map(article =>
-      parseArticle($, article)
-    )
-    return addData(entries, 'com.toscrape.books').then(() =>
-      saveFiles(entries, fields)
-    )
+function getBulletinsList(cesuNum) {
+  const url = baseUrl + 'cesuwebdec/employeurs/' + cesuNum + '/bulletinsSalaire?numInterneSalarie=&dtDebutRecherche=20130101&dtFinRecherche=20500101&numStart=0&nbAffiche=1000&numeroOrdre=0'
+  return request({
+    url: url,
+    json: true
   })
+    .then(body => body.listeObjets.filter(item => item.telechargeable === true ))
 }
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance // and return an array of js objects which will be saved to the cozy by addData (https://github.com/cozy/cozy-konnector-libs/blob/master/docs/api.md#module_addData)
-// and saveFiles (https://github.com/cozy/cozy-konnector-libs/blob/master/docs/api.md#savefiles)
-function parseArticle($, article) {
-  const $article = $(article)
-  const title = $article.find('h3 a').attr('title')
-  return {
-    title,
-    price: normalizePrice($article.find('.price_color').text()),
-    url: `${baseUrl}/${$article.find('h3 a').attr('href')}`,
-    // when it finds a fileurl attribute, saveFiles will save this file to the cozy with a filename
-    // name
-    fileurl: `${baseUrl}/${$article.find('img').attr('src')}`,
-    filename: `${title}.jpg`
-  }
-}
+function constructList(jsonList) {
+  var files = []
+  for f in jsonList {
+    console.log(f)
+  } 
 
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.trim().replace('£', ''))
 }
