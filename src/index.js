7d14c603611b1c6e1a889429d1f2758fcc363655
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2024-07-02 17:52:28 +0200

    Clean template's code

--- src/index.js
+++ src/index.js
@@ -1,67 +1,11 @@
 import { ContentScript } from 'cozy-clisk/dist/contentscript'
 import Minilog from '@cozy/minilog'
 const log = Minilog('ContentScript')
-Minilog.enable()
+Minilog.enable('cesuCCC')
 
-const baseUrl = 'https://toscrape.com'
-const defaultSelector = "a[href='http://quotes.toscrape.com']"
-const loginLinkSelector = `[href='/login']`
-const logoutLinkSelector = `[href='/logout']`
+const BASE_URL = 'https://www.cesu.urssaf.fr'
 
 class TemplateContentScript extends ContentScript {
-  async navigateToLoginForm() {
-    this.log('info', '🤖 navigateToLoginForm')
-    await this.goto(baseUrl)
-    await this.waitForElementInWorker(defaultSelector)
-    await this.runInWorker('click', defaultSelector)
-    // wait for both logout or login link to be sure to check authentication when ready
-    await Promise.race([
-      this.waitForElementInWorker(loginLinkSelector),
-      this.waitForElementInWorker(logoutLinkSelector)
-    ])
-  }
-
-  onWorkerEvent({ event, payload }) {
-    if (event === 'loginSubmit') {
-      this.log('info', 'received loginSubmit, blocking user interactions')
-      this.blockWorkerInteractions()
-    } else if (event === 'loginError') {
-      this.log(
-        'info',
-        'received loginError, unblocking user interactions: ' + payload?.msg
-      )
-      this.unblockWorkerInteractions()
-    }
-  }
-
-  async ensureAuthenticated({ account }) {
-    this.bridge.addEventListener('workerEvent', this.onWorkerEvent.bind(this))
-    this.log('info', '🤖 ensureAuthenticated')
-    if (!account) {
-      await this.ensureNotAuthenticated()
-    }
-    await this.navigateToLoginForm()
-    const authenticated = await this.runInWorker('checkAuthenticated')
-    if (!authenticated) {
-      this.log('info', 'Not authenticated')
-      await this.showLoginFormAndWaitForAuthentication()
-    }
-    this.unblockWorkerInteractions()
-    return true
-  }
-
-  async ensureNotAuthenticated() {
-    this.log('info', '🤖 ensureNotAuthenticated')
-    await this.navigateToLoginForm()
-    const authenticated = await this.runInWorker('checkAuthenticated')
-    if (!authenticated) {
-      return true
-    }
-
-    await this.clickAndWait(logoutLinkSelector, loginLinkSelector)
-    return true
-  }
-
   onWorkerReady() {
     window.addEventListener('DOMContentLoaded', () => {
       const button = document.querySelector('input[type=submit]')
@@ -80,13 +24,45 @@ class TemplateContentScript extends ContentScript {
     })
   }
 
-  async checkAuthenticated() {
-    return Boolean(document.querySelector(logoutLinkSelector))
+  onWorkerEvent({ event, payload }) {
+    if (event === 'loginSubmit') {
+      this.log('info', 'received loginSubmit, blocking user interactions')
+      this.blockWorkerInteractions()
+    } else if (event === 'loginError') {
+      this.log(
+        'info',
+        'received loginError, unblocking user interactions: ' + payload?.msg
+      )
+      this.unblockWorkerInteractions()
+    }
   }
+
+  async navigateToLoginForm() {
+    this.log('info', '🤖 navigateToLoginForm')
+    await this.goto(BASE_URL)
+  }
+
+  async ensureAuthenticated({ account }) {
+    this.bridge.addEventListener('workerEvent', this.onWorkerEvent.bind(this))
+    this.log('info', '🤖 ensureAuthenticated')
+    await this.navigateToLoginForm()
+    return true
+  }
+
+  async ensureNotAuthenticated() {
+    this.log('info', '🤖 ensureNotAuthenticated')
+    await this.navigateToLoginForm()
+    const authenticated = await this.runInWorker('checkAuthenticated')
+    if (!authenticated) {
+      return true
+    }
+    return true
+  }
+
+  async checkAuthenticated() {}
 
   async showLoginFormAndWaitForAuthentication() {
     log.debug('showLoginFormAndWaitForAuthentication start')
-    await this.clickAndWait(loginLinkSelector, '#username')
     await this.setWorkerState({ visible: true })
     await this.runInWorkerUntilTrue({
       method: 'waitForAuthenticated'
@@ -96,16 +72,6 @@ class TemplateContentScript extends ContentScript {
 
   async fetch(context) {
     this.log('info', '🤖 fetch')
-    await this.goto('https://books.toscrape.com')
-    await this.waitForElementInWorker('#promotions')
-    const bills = await this.runInWorker('parseBills')
-
-    await this.saveBills(bills, {
-      contentType: 'image/jpeg',
-      fileIdAttributes: ['filename'],
-      context
-    })
-
     const identity = await this.runInWorker('parseIdentity')
     await this.saveIdentity(identity)
   }
@@ -116,48 +82,9 @@ class TemplateContentScript extends ContentScript {
       sourceAccountIdentifier: 'defaultTemplateSourceAccountIdentifier'
     }
   }
-
-  async parseBills() {
-    const articles = document.querySelectorAll('article')
-    return Array.from(articles).map(article => ({
-      amount: normalizePrice(article.querySelector('.price_color')?.innerHTML),
-      date: '2024-01-01', // use a fixed date to avoid the multiplication of bills
-      vendor: 'template',
-      filename: article.querySelector('h3 a')?.getAttribute('title'),
-      fileurl:
-        'https://books.toscrape.com/' +
-        article.querySelector('img')?.getAttribute('src')
-    }))
-  }
-
-  async parseIdentity() {
-    const contact = {
-      name: {
-        givenName: 'John',
-        familyName: 'Doe'
-      },
-      address: [
-        {
-          street: '2 rue du moulin',
-          postcode: '00000',
-          city: 'Paris',
-          formattedAddress: '2 rue du moulin 00000 Paris'
-        }
-      ],
-      email: [{ address: 'mail@mail.com' }]
-    }
-    return { contact }
-  }
-}
-
-// Convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
 }
 
 const connector = new TemplateContentScript()
-connector
-  .init({ additionalExposedMethodsNames: ['parseBills', 'parseIdentity'] })
-  .catch(err => {
-    log.warn(err)
-  })
+connector.init({ additionalExposedMethodsNames: [''] }).catch(err => {
+  log.warn(err)
+})
